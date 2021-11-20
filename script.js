7f35e332aacaea7f97373d4dfd6875133db151eb
sreedevi-sree <sreedevifsd@gmail.com>
2021-11-20 23:41:41 +0530

    first commit

--- script.js
+++ script.js
@@ -1,18 +1,35 @@
-document.getElementById("button1").addEventListener("click", getApi);
-function getApi() {
-  fetch("https://cdn.jsdelivr.net/gh/fawazahmed0/quran-api@1/editions.json").then((response) => {
-    response.json().then((data) => {
-      // console.log(data);
-      let result = "";
-      for(var x in data)
+// set the target element
+const fetchDataBtn = document.querySelector("#fetchdata");
+
+// get data from API
+async function getData() {
+  let result="";
+  try {
+    const res = await fetch("https://api.github.com/users/sreedevi-sree/repos");
+    const jsonResult = await res.json();
+    let img= jsonResult[0].owner.avatar_url;
+for(i=0;i<jsonResult.length;i++)
 {
-  result+=data[x].language+" ";  
- 
-}
-console.log(result);
-     
-      document.getElementById("output").innerHTML = result;
-    });
-  });
+  result += `
+  Repository= ${jsonResult[i].name}
+  fork count= ${jsonResult[i].forks_count}
+  star count = ${jsonResult[i].stargazers_count}
+  Repository link= ${jsonResult[i].url}
+  `; 
 }
 
+// sets the content of #result div     
+    document.getElementById("result").innerText=result;
+
+// sets the image content of #image div
+    document.getElementById("image").src=img;
+  } 
+
+  // To catch error in the console
+    catch (error) {
+    console.log(error);
+  }
+}
+
+// add event listener for #fetchdata button
+fetchDataBtn.addEventListener("click", getData);
